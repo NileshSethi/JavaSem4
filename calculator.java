6cad6c8a62550ee4ae44a3b96221b4165bea18a9
Nilesh_Sethi <sethinileshsethi@gmail.com>
2026-01-16 01:22:11 +0530

    Restored file

---
+++ calculator.java
@@ -0,0 +1,10 @@
+public class calculator{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 15; 
+
+        int sum = a + b;
+        
+        System.out.println("Addition:" + sum);
+    }
+}
