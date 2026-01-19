02564cc645174ba08ff39c85640efb2df5720aff
Nilesh_Sethi <sethinileshsethi@gmail.com>
2026-01-19 09:44:58 +0530

    d3work

--- dailywork/d3.java
+++ dailywork/d3.java
@@ -2,9 +2,14 @@ class Student{
     String name;
     int age;
 
-    Student(){
-        name = "Nilesh";
-        age = 19;
+    // Student(){                  [default constructor]
+    //     name = "Unknown";
+    //     age = 0;
+    // }
+
+    Student(String s , int a){     // parameterized constructor
+        name = s;
+        age = a;
     }
     void display(){
         System.out.println("Name: " + name);
@@ -13,7 +18,7 @@ class Student{
 } 
 public class d3 {
     public static void main(String[] args) {
-        Student student1 = new Student();
+        Student student1 = new Student("Nilesh", 19);
         student1.display();
     }
 }
