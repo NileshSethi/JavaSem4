ecd1dc97035a4138739f0e6a358abd6bc8b20283
Nilesh_Sethi <sethinileshsethi@gmail.com>
2026-01-19 10:35:31 +0530

    d3work

--- dailywork/d3.java
+++ dailywork/d3.java
@@ -26,7 +26,8 @@ public class d3 {
 */
 
 
-class Counter{
+// static variable example
+/* class Counter{
     static int count = 0;
 
     Counter(){
@@ -39,5 +40,43 @@ public class d3 {
         Counter c1 = new Counter();
         Counter c2 = new Counter();
         Counter c3 = new Counter();
+    }
+}
+*/
+
+
+/* 
+//instance variable example
+class d3{
+    String name;
+    int id;
+    public static void main(String[] args) {
+        d3 s1 = new d3();  
+        d3 s2 = new d3();                        // d3 = Student 
+        s1.name = "Nilesh";
+        s1.id = 121;
+        s2.id = 122;
+        s2.name = "Amit";
+        System.out.println("Name: " + s1.name);
+        System.out.println("ID: " + s1.id);
+        System.out.println("Name: " + s2.name);
+        System.out.println("ID: " + s2.id);
+    }
+}
+*/
+
+
+//instance method example
+class d3{
+    String name;
+
+    void display(){
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        d3 p = new d3();
+        p.name = "Nilesh";
+        p.display();
     }
 }
