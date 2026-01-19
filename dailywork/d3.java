a70f1e85156223ced917b4024f39305aae4ca06e
Nilesh_Sethi <sethinileshsethi@gmail.com>
2026-01-19 10:00:44 +0530

    d3work

--- dailywork/d3.java
+++ dailywork/d3.java
@@ -1,3 +1,4 @@
+/* 
 class Student{
     String name;
     int age;
@@ -20,5 +21,23 @@ public class d3 {
     public static void main(String[] args) {
         Student student1 = new Student("Nilesh", 19);
         student1.display();
+    }
+}
+*/
+
+
+class Counter{
+    static int count = 0;
+
+    Counter(){
+        count++;
+        System.out.println("Counter value: " + count);
+    }
+}
+public class d3 {
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
     }
 }
