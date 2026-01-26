6d65a63c665caf3784ba18fac123fb550bd25cb8
Nilesh_Sethi <sethinileshsethi@gmail.com>
2026-01-26 18:42:52 +0530

    h3work

---
+++ homework/h3work3.java
@@ -0,0 +1,21 @@
+class A {
+    int x = 10;
+}
+
+class B {
+    int y = 20;
+}
+
+public class h3work3 {
+
+    public static void main(String[] args) {
+
+        A objA = new A();
+        B objB = new B();
+        h3work3 objC = new h3work3();
+
+        System.out.println(objA.x);
+        System.out.println(objB.y);
+        System.out.println("This is Class h3work3");
+    }
+}
