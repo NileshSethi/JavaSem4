1878b1fc8da2aff0e313cc810135708367985eb1
Nilesh_Sethi <sethinileshsethi@gmail.com>
2026-02-07 23:58:51 +0530

    fixed

---
+++ dailywork/d7/d7.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class d7 {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        System.out.println("Fruits: " + fruits);
+        fruits.remove("Banana"); // Remove element
+        System.out.println("After removing Banana: " + fruits);
+        System.out.println("Size: " + fruits.size()); // Get size
+        System.out.println("Contains Apple? : " + fruits.contains("Apple"));
+    }
+}
+
+
