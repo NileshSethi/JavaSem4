56f77f575ddf8f7db5b115a9c68bc7b043375554
Nilesh_Sethi <82396651+NileshSethi@users.noreply.github.com>
2026-02-20 15:47:52 +0530

    d8 work

---
+++ dailywork/d8/d8.java
@@ -0,0 +1,87 @@
+/* 
+public class d8{
+    public static void main(String[] args) {
+        try {
+            int n[] = new int[5];
+            int divisor = 0; 
+            for (int i=0; i<n.length ; i++) {
+                int res = n[i] / divisor;
+                System.out.println(res); 
+            }
+        } 
+        catch (ArithmeticException e) {
+            throw new RuntimeException 
+                ("Error: Cannot divide by zero. " + e);
+        }
+    }
+}
+
+*/
+
+
+
+/*
+// NULL pointer exception in java
+class d8 {
+    public static void main(String[] args) {
+        String s = "";
+        try {
+            System.out.println(getLength(s));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(
+                "IllegalArgumentException caught");
+        }
+        
+        try {
+            System.out.println(getLength(s));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(
+                "IllegalArgumentException caught");
+        }
+        //s = "";
+        s = null;
+        try {
+            System.out.println(getLength(s));
+        }
+         
+        catch (IllegalArgumentException e) {
+            System.out.println(
+                e);
+        }
+        
+    }
+
+    public static int getLength(String s)
+    {
+        if (s == null)
+            throw new IllegalArgumentException(
+                "The argument cannot be null");
+
+        return s.length();
+    }
+}
+*/
+
+// Superclass without exception declaration
+class SuperClass {
+    void method() {
+        System.out.println("Superclass method executed");
+    }
+}
+class SubClass extends SuperClass {
+    @Override
+    void method() throws ArithmeticException {
+        System.out.println("SubClass method executed");
+        throw new ArithmeticException("Exception in SubClass");
+    }
+    public static void main(String[] args) {
+        SuperClass s = new SubClass();
+        try {
+            s.method();
+        } catch (ArithmeticException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+        }
+    }
+}
